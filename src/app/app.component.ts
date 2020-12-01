cdf76a576004550e25acf9f984db46c337088e4d
Tadreik Campbell <tadreik@gmail.com>
2020-12-01 13:26:01 -0500

    setup complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    // animation triggers go here
+  ]
 })
 export class AppComponent {
-  title = 'FoodieThingWeb';
+  title = 'Foodie Thing';
 }
