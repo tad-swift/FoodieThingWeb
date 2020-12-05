f65f3c1ccfde89791e7c65f45836065043d4d49e
Tadreik Campbell <tadreik@gmail.com>
2020-12-05 11:01:29 -0500

    u

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +13,7 @@ import { RouterModule } from '@angular/router';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    RouterModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,9 @@
+import {NgModule} from '@angular/core';
+import {MdcButtonModule} from '@angular-mdc/web/button';
+
+@NgModule({
+  exports: [
+    MdcButtonModule,
+  ]
+})
+export class MaterialModule {}
